d6a0298a0b105111cd7b17c8c539205967be354c
MrGramps91 <texas9137@gmail.com>
2017-09-26 08:08:39 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,57 @@
 package application;
 	
+import java.io.File;
+import java.net.MalformedURLException;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		Player player = new Player("file:///C:/"); //used temporarily until a 'file select option is incorporated//
-		Scene scene = new Scene(player,1280,760, Color.BLACK); //used temporarily until 'automatic video scaling is incorporated//
+	Player player;
+	FileChooser fileChooser;
+	public void start(final Stage primaryStage) {
+		
+		MenuItem open = new MenuItem("Open");
+		Menu file = new Menu("File");
+		MenuBar menu = new MenuBar();
+		
+		file.getItems().add(open);
+		menu.getMenus().add(file);
+		
+		open.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent e){
+				player.player.pause();
+				fileChooser = new FileChooser();
+				File file = fileChooser.showOpenDialog(primaryStage);
+				if(file != null){
+					try {
+						player = new Player(file.toURI().toURL().toExternalForm());
+						Scene scene = new Scene(player,1280,760, Color.BLACK );
+						primaryStage.setScene(scene);
+					} catch (MalformedURLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+			
+		});
+		
+		Player player = new Player("file:///C:/Users/Public/Music/Sample%20Music/Sleep%20Away.mp3"); //figure put the proper syntx for opening files
+		player.setTop(menu);
+		Scene scene = new Scene(player,1280,780, Color.BLACK);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
